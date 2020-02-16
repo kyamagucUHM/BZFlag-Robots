3ee1a90d59f1327e085e847935660fd354381cf5
Jiajun Kang <46578408+JiajunK@users.noreply.github.com>
2020-02-15 18:23:01 -1000

    Add files via upload

---
+++ RobotPlayer.h
@@ -0,0 +1,87 @@
+/* bzflag
+ * Copyright (c) 1993-2018 Tim Riker
+ *
+ * This package is free software;  you can redistribute it and/or
+ * modify it under the terms of the license found in the file
+ * named COPYING that should have accompanied this file.
+ *
+ * THIS PACKAGE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+/*
+ *
+ */
+
+#ifndef BZF_ROBOT_PLAYER_H
+#define BZF_ROBOT_PLAYER_H
+
+#include "common.h"
+
+/* system interface headers */
+#include <vector>
+
+/* interface header */
+#include "LocalPlayer.h"
+
+/* local interface headers */
+#include "Region.h"
+#include "RegionPriorityQueue.h"
+#include "ServerLink.h"
+
+
+class RobotPlayer : public LocalPlayer
+{
+public:
+    RobotPlayer(const PlayerId&,
+                const char* name, ServerLink*,
+                const char* _motto);
+
+    float       getTargetPriority(const Player*) const;
+    const Player*   getTarget() const;
+    void        setTarget(const Player*);
+    static void     setObstacleList(std::vector<BzfRegion*>*);
+
+    void        restart(const float* pos, float azimuth);
+    void        explodeTank();
+
+private:
+    float getCenterOfMass(float hoodSize, float teamCom[3]);
+    float getAlignment(float hoodSize, float teamAlign[3], float* avgAzi);
+    float getSeparation(float hoodSize, float teamSep[3]);
+    void getMyBase(TeamColor teamColor, float location[3]);
+    bool keepFlag();
+    void findFlag(float location[3]);
+    //void        doUpdate(float dt);
+    void        doUpdateMotion(float dt);
+    BzfRegion*      findRegion(const float p[2], float nearest[2]) const;
+    float       getRegionExitPoint(
+        const float p1[2], const float p2[2],
+        const float a[2], const float targetPoint[2],
+        float mid[2], float& priority);
+    void       findPath(RegionPriorityQueue& queue,
+                        BzfRegion* region, BzfRegion* targetRegion,
+                        const float targetPoint[2], int mailbox);
+
+    void       projectPosition(const Player *targ,const float t,float &x,float &y,float &z) const;
+    void       getProjectedPosition(const Player *targ, float *projpos) const;
+
+private:
+    const Player*   target;
+    std::vector<RegionPoint>    path;
+    int         pathIndex;
+    float       timerForShot;
+    bool        drivingForward;
+    static std::vector<BzfRegion*>* obstacleList;
+};
+
+#endif // BZF_ROBOT_PLAYER_H
+
+// Local Variables: ***
+// mode: C++ ***
+// tab-width: 4 ***
+// c-basic-offset: 4 ***
+// indent-tabs-mode: nil ***
+// End: ***
+// ex: shiftwidth=4 tabstop=4
